368cb29b975265d962ce214b4705c5c7ad3ec9e5
kopalgarg <kopalgarg24@gmail.com>
2022-03-11 10:36:21 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File   : __init__.py
+# Created by raeidsaqur at 2021-12-30
+# Email  : raeidsaqur@cs.toronto.edu
+#
+# This file is part of ${PROJECT_NAME}.
+# Distributed under terms of the ${LICENSE}.
+# https://github.com/${REPO_ADDR}
