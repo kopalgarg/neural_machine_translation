4f78c0cea5eba8f032476a26e98ec5e16d09f1cb
kopalgarg <kopalgarg24@gmail.com>
2022-03-11 10:38:13 -0500

    copy of my code

---
+++ copy_of_my_code/a2_bleu_score copy.py
@@ -0,0 +1,145 @@
+'''
+This code is provided solely for the personal and private use of students
+taking the CSC401H/2511H course at the University of Toronto. Copying for
+purposes other than this use is expressly prohibited. All forms of
+distribution of this code, including but not limited to public repositories on
+GitHub, GitLab, Bitbucket, or any other online platform, whether as given or
+with any changes, are expressly prohibited.
+
+Authors: Sean Robertson, Jingcheng Niu, Zining Zhu, and Mohamed Abdall
+Updated by: Raeid Saqur <raeidsaqur@cs.toronto.edu>
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) 2022 University of Toronto
+'''
+
+'''Calculate BLEU score for one reference and one hypothesis
+
+You do not need to import anything more than what is here
+'''
+
+from math import exp  # exp(x) gives e^x
+from typing import List, Sequence, Iterable
+
+
+def grouper(seq:Sequence[str], n:int) -> List:
+    '''Extract all n-grams from a sequence
+
+    An n-gram is a contiguous sub-sequence within `seq` of length `n`. This
+    function extracts them (in order) from `seq`.
+
+    Parameters
+    ----------
+    seq : sequence
+        A sequence of words or token ids representing a transcription.
+    n : int
+        The size of sub-sequence to extract.
+
+    Returns
+    -------
+    ngrams : list
+    '''
+    ngrams = []
+
+    for i in range(0, len(seq) - n + 1):
+        ngrams.append(" ".join(seq[i: i + n]))
+
+    return ngrams
+
+
+def n_gram_precision(reference:Sequence[str], candidate:Sequence[str], n:int) -> float:
+    '''Calculate the precision for a given order of n-gram
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The order of n-gram precision to calculate
+
+    Returns
+    -------
+    p_n : float
+        The n-gram precision. In the case that the candidate has length 0,
+        `p_n` is 0.
+    '''
+    p_n = 0
+
+    candidate_ngrams = grouper(candidate, n)
+    reference_ngrams = grouper(reference, n)
+    count = 0
+    if len(candidate) > 0:
+        for c in candidate_ngrams:
+            if c in reference_ngrams:
+                count+=1
+        if len(candidate_ngrams)>0:
+            p_n = count/len(candidate_ngrams)
+    else:
+        p_n = 0
+
+    return p_n
+
+
+def brevity_penalty(reference:Sequence[str], candidate:Sequence[str]) -> float:
+    '''Calculate the brevity penalty between a reference and candidate
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+
+    Returns
+    -------
+    BP : float
+        The brevity penalty. In the case that the candidate transcription is
+        of 0 length, `BP` is 0.
+    '''
+    BP = 0
+
+    if len(candidate) > 0:
+        br = len(reference)/len(candidate)
+        if br < 1:
+            BP  = 1
+        else:
+            BP = exp(1-br)
+    else:
+        BP = 0
+
+    return BP
+
+def BLEU_score(reference:Sequence[str], candidate:Sequence[str], n) -> float:
+    '''Calculate the BLEU score
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The maximum order of n-gram precision to use in the calculations,
+        inclusive. For example, ``n = 2`` implies both unigram and bigram
+        precision will be accounted for, but not trigram.
+
+    Returns
+    -------
+    bleu : float
+        The BLEU score
+    '''
+    p_scores = 1
+    for i in range(1, n+1):
+        prec = n_gram_precision(reference, candidate, i)
+        p_scores *= prec
+    
+    BP = brevity_penalty(reference, candidate)
+
+    bleu = BP * (p_scores ** (1 / n))
+
+    return bleu
